bbd31d2d6a2524bd4c3b35029344e4c8552a7db0
Sijan Malla <sijanstudycomputers@gmail.com>
2020-05-29 12:38:33 -0500

    started learning datastructures with polymorphism first by creating a simple interface to find out average of a array input

--- Interfaces/InterfaceExample.java
+++ Interfaces/InterfaceExample.java
@@ -1,1 +1,12 @@
-Hi
+//writing a generic interface 
+public static double average(Measurable[] objects){
+  if (objects.length == 0){
+    return 0;
+  }
+  double sum = 0;
+  for (Measurable obj: objects){
+    sum = sum+obj.getMeasure();
+  }
+
+  return sum/objects.length();
+}
